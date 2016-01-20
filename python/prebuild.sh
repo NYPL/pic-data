d7cdced36432b803467d91480a437b9105e18035
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-01-20 14:51:10 -0500

    :space_invader: cleanup

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -3,11 +3,6 @@
 echo ""
 echo "#### STARTED TRAVIS-CI MAGIC"
 
-# export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
-
-# git config --global user.name "travis-bot"
-# git config --global user.email "travis"
-
 echo "Creating text files"
 python ./python/data_converter.py
 
