27e877e01cd4cb1d42c0d26ab5259454836a6735
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-01-20 14:34:31 -0500

    :space_invader: removed git things in python

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -3,50 +3,50 @@
 echo ""
 echo "#### STARTED TRAVIS-CI MAGIC"
 
-export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
+# export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
 
-git config --global user.name "travis-bot"
-git config --global user.email "travis"
+# git config --global user.name "travis-bot"
+# git config --global user.email "travis"
 
 echo "Creating text files"
 python ./python/data_converter.py
 
-echo ""
-echo "Clone the app repo"
-git clone $REPO_URL _cloned
+# echo ""
+# echo "Clone the app repo"
+# git clone $REPO_URL _cloned
 
-cd _cloned
+# cd _cloned
 
-git remote add origin $REPO_URL
+# git remote add origin $REPO_URL
 
-echo ""
-echo "Add the new files"
-echo "Before:"
-ls -lh ./public/assets/csv
-cp ../csv/*.txt ./public/assets/csv
-cp ../csv/*.csv ./public/assets/csv
-echo ""
-echo "After:"
-ls -lh ./public/assets/csv
+# echo ""
+# echo "Add the new files"
+# echo "Before:"
+# ls -lh ./public/assets/csv
+# cp ../csv/*.txt ./public/assets/csv
+# cp ../csv/*.csv ./public/assets/csv
+# echo ""
+# echo "After:"
+# ls -lh ./public/assets/csv
 
-echo ""
-echo "Status"
-git status
+# echo ""
+# echo "Status"
+# git status
 
-echo ""
-echo "Adding new files"
-git add public/assets/csv
+# echo ""
+# echo "Adding new files"
+# git add public/assets/csv
 
-git commit -m ":rocket: new deploy from travis-ci"
+# git commit -m ":rocket: new deploy from travis-ci"
 
-echo ""
-echo "Running indexer"
-python ../python/index_builder.py
+# echo ""
+# echo "Running indexer"
+# python ../python/index_builder.py
 
-echo ""
-echo "Push to update app repo"
+# echo ""
+# echo "Push to update app repo"
 
-git push origin master
+# git push origin master
 
 echo ""
 echo "#### DEPLOY COMPLETE"
