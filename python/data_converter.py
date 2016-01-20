8e2a4626a3bf3f3dcdba9a16abf25a488d9b774f
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-01-20 18:02:10 -0500

    :space_invader: refactored python code

--- python/data_converter.py
+++ python/data_converter.py
@@ -4,52 +4,11 @@ import csv
 import os
 import re
 
+from pic import Converter
+
 import timeit
 import warnings
 warnings.filterwarnings('ignore')
-
-def remove_bom(row):
-    cleaned = {}
-    for k, v in row.iteritems():
-        if (k.find('\xef\xbb\xbf')==0):
-            kk = re.sub(r'\xef\xbb\xbf', r'', k)
-            cleaned[kk] = v
-        else:
-            cleaned[k] = v
-    return cleaned
-
-def convertWhitespace(string):
-    string = string.replace('\xc2\xa0', ' ') # because WTF MSSQL Server!?
-    string = string.replace('\u00a0', ' ') # because WTF MSSQL Server!?
-    return string
-
-def remove_zeroes(string):
-    while string.endswith("0") and len(string)>1:
-        string = string[0:-1]
-    return string
-
-def compress_address(remarks):
-    precision = 4
-    temp = remarks.replace(" ", "").split(",")
-    lat = temp[0]
-    if lat.find(".") != -1:
-        lat = lat.split(".")
-        lat[1] = remove_zeroes(lat[1][:precision])
-        temp[0] = lat[0] + "." + lat[1]
-    lon = temp[1]
-    if lon.find(".") != -1:
-        lon = lon.split(".")
-        lon[1] = remove_zeroes(lon[1][:precision])
-        temp[1] = lon[0] + "." + lon[1]
-    return temp
-
-def process_csv(filename):
-    basepath = os.environ['BASEPATH']
-    readpath = basepath+filename
-    print "loaded " + readpath
-    response = open(readpath)
-    reader = csv.DictReader(response)
-    return reader
 
 def generate_base_locations():
     """
@@ -65,9 +24,9 @@ def generate_base_locations():
     heights = []
     every_row = []
     for row in reader:
-        row = remove_bom(row)
+        row = Converter.remove_bom(row)
         remarks = row['Remarks']
-        remarks = convertWhitespace(remarks)
+        remarks = Converter.convert_whitespace(remarks)
         if remarks == None:
             print "No remarks:" + row['ConstituentID'] + ":" + remarks
             continue
@@ -97,7 +56,7 @@ def generate_base_locations():
     for_real_sorted_every_row.extend(other_addresses)
     for_real_sorted_every_row.extend(died_addresses)
     for row in for_real_sorted_every_row:
-        address = compress_address(row['Remarks'])
+        address = Converter.compress_address(row['Remarks'])
         height = 0
         if len(address) > 2:
             height = address.pop()
@@ -123,33 +82,6 @@ def generate_base_locations():
     text_file = open(output, "w")
     text_file.write(locations)
     text_file.close()
-
-def sort_addresses(addresses):
-    sortedaddresses = []
-    born = {}
-    died = {}
-    if len(addresses) <= 1:
-        return addresses
-    ## sort by date
-    addresses = sorted(addresses, key=lambda d: d['BeginDate'])
-    for add in addresses:
-        # put the active/biz ones
-        if (add['AddressTypeID'] == '7' or add['AddressTypeID'] == '2'):
-            sortedaddresses.append(add)
-        # find born if any
-        if (add['AddressTypeID'] == '5'):
-            born = add
-        # find died if any
-        if (add['AddressTypeID'] == '6'):
-            died = add
-    # prepend born
-    if (born):
-        sortedaddresses.insert(0,born)
-    # append died
-    if (died):
-        sortedaddresses.append(died)
-    return sortedaddresses
-
 
 def main():
     start = timeit.default_timer()
--- python/pic.py
+++ python/pic.py
@@ -1,3 +1,6 @@
+import csv
+import re
+
 from elasticsearch_dsl import analyzer, DocType, Object, Nested, String, Integer, Long
 
 accent_analyzer = analyzer('accent_analyzer',
@@ -74,3 +77,78 @@ class Constituent(DocType):
             'TermID' : String()
         }
     )
+
+class Converter:
+
+    @staticmethod
+    def remove_bom(row):
+        cleaned = {}
+        for k, v in row.iteritems():
+            if (k.find('\xef\xbb\xbf')==0):
+                kk = re.sub(r'\xef\xbb\xbf', r'', k)
+                cleaned[kk] = v
+            else:
+                cleaned[k] = v
+        return cleaned
+
+    @staticmethod
+    def convert_whitespace(string):
+        string = string.replace('\xc2\xa0', ' ') # because WTF MSSQL Server!?
+        string = string.replace('\u00a0', ' ') # because WTF MSSQL Server!?
+        return string
+
+    @staticmethod
+    def remove_zeroes(string):
+        while string.endswith("0") and len(string)>1:
+            string = string[0:-1]
+        return string
+
+    @staticmethod
+    def compress_address(remarks):
+        precision = 4
+        temp = remarks.replace(" ", "").split(",")
+        lat = temp[0]
+        if lat.find(".") != -1:
+            lat = lat.split(".")
+            lat[1] = Converter.remove_zeroes(lat[1][:precision])
+            temp[0] = lat[0] + "." + lat[1]
+        lon = temp[1]
+        if lon.find(".") != -1:
+            lon = lon.split(".")
+            lon[1] = Converter.remove_zeroes(lon[1][:precision])
+            temp[1] = lon[0] + "." + lon[1]
+        return temp
+
+    @staticmethod
+    def process_csv(filename):
+        print "loaded " + filename
+        response = open(filename)
+        reader = csv.DictReader(response)
+        return reader
+
+    @staticmethod
+    def sort_addresses(addresses):
+        sortedaddresses = []
+        born = {}
+        died = {}
+        if len(addresses) <= 1:
+            return addresses
+        ## sort by date
+        addresses = sorted(addresses, key=lambda d: d['BeginDate'])
+        for add in addresses:
+            # put the active/biz ones
+            if (add['AddressTypeID'] == '7' or add['AddressTypeID'] == '2'):
+                sortedaddresses.append(add)
+            # find born if any
+            if (add['AddressTypeID'] == '5'):
+                born = add
+            # find died if any
+            if (add['AddressTypeID'] == '6'):
+                died = add
+        # prepend born
+        if (born):
+            sortedaddresses.insert(0,born)
+        # append died
+        if (died):
+            sortedaddresses.append(died)
+        return sortedaddresses
