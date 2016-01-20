6a74698994939c1b0976264d79b26a92122863e5
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-01-20 14:50:07 -0500

    :space_invader: uncommented indexing

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -11,9 +11,9 @@ echo "#### STARTED TRAVIS-CI MAGIC"
 echo "Creating text files"
 python ./python/data_converter.py
 
-# echo ""
-# echo "Running indexer"
-# python ../python/index_builder.py
+echo ""
+echo "Running indexer"
+python ../python/index_builder.py
 
 echo ""
 echo "#### DEPLOY COMPLETE"
