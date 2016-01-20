c9a149c2e8dea4914547822ac2597c44ef71d90a
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-01-20 15:39:54 -0500

    :space_invader: fixed indexing / using before_deploy

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -8,14 +8,7 @@ python ./python/data_converter.py
 
 echo ""
 echo "Running indexer"
-python ../python/index_builder.py
+python ./python/index_builder.py
 
 echo ""
-echo "Cleaning extraneous files before S3 deploy"
-rm ./python/*
-rm README.md
-rm LICENSE
-rmdir python
-
-echo ""
-echo "#### DEPLOY COMPLETE"
+echo "#### INDEXING COMPLETE"
