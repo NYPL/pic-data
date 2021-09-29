aa7ebd533c4c36499d960622092b690f9e065e7f
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2021-09-28 23:54:25 -0400

    back to env aws secret / better prebuild

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -3,18 +3,24 @@
 echo ""
 echo "#### STARTED TRAVIS-CI MAGIC"
 
-# echo "Testing connection"
-# python ./python/test_connection.py
+if [ "$1" = "test" ]
+then
+    echo "Testing connection"
+    python ./python/test_connection.py
+fi
 
-echo "Creating text files"
-python ./python/data_converter.py
+if [ -z $1 ]
+then
+    echo "Creating text files"
+    python ./python/data_converter.py
 
-echo "Getting minimum year"
-python ./python/minimum_year.py
+    echo "Getting minimum year"
+    python ./python/minimum_year.py
 
-echo ""
-echo "Running indexer"
-python ./python/index_builder.py
+    echo ""
+    echo "Running indexer"
+    python ./python/index_builder.py
+fi
 
 echo ""
 echo "#### INDEXING COMPLETE"
